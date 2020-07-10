c9324a46a23b6357f45525fa0e403ea38542f868
jakobkhansen <jakob.hansen@hotmail.no>
2020-07-10 21:03:57 +0200

    Fix custom hotkey file bug

--- src/hotkey-menu-launcher.ts
+++ src/hotkey-menu-launcher.ts
@@ -1,161 +1,181 @@
-import { remote, app, BrowserWindow } from 'electron';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as pkgDir from 'pkg-dir';
+import { remote, app, BrowserWindow } from "electron"
+import * as fs from "fs"
+import * as path from "path"
+import * as pkgDir from "pkg-dir"
 
 interface MenuOptions {
-  width?: number;
-  height?: number;
-  hotkeys?: Hotkey[];
-  css?: string;
-  savefile?: string;
+	width?: number
+	height?: number
+	hotkeys?: Hotkey[]
+	css?: string
+	savefile?: string
 }
 
-const DEFAULT_WIDTH = 300;
-const DEFAULT_HEIGHT = 300;
-const DEFAULT_SAVEFILE = 'hotkey.json';
+const DEFAULT_WIDTH = 300
+const DEFAULT_HEIGHT = 300
+const DEFAULT_SAVEFILE = "./hotkeys.json"
 
 export class HotkeyMenu {
-  hotkeys: Hotkey[] = [];
-  private options: MenuOptions = {};
+	hotkeys: Hotkey[] = []
+	private options: MenuOptions = {}
 
-  /*
-   * width/height: Dimensions of HotkeyMenu
-   * hotkeys: Array of Hotkey objects
-   * css: Custom css to overload the default css
-   */
-  constructor(options: MenuOptions) {
-    this.options.width = options.width ? options.width : DEFAULT_WIDTH;
-    this.options.height = options.height ? options.height : DEFAULT_HEIGHT;
-    this.options.savefile = options.savefile ? options.savefile : DEFAULT_SAVEFILE;
+	/*
+	 * width/height: Dimensions of HotkeyMenu
+	 * hotkeys: Array of Hotkey objects
+	 * css: Custom css to overload the default css
+	 */
+	constructor(options: MenuOptions) {
+		this.options.width = options.width ? options.width : DEFAULT_WIDTH
+		this.options.height = options.height ? options.height : DEFAULT_HEIGHT
+		this.options.savefile = options.savefile ? options.savefile : DEFAULT_SAVEFILE
 
-    if (options.hotkeys) {
-      options.hotkeys.forEach((f) => {
-        this.addHotkey(f);
-      });
+		if (options.hotkeys) {
+			options.hotkeys.forEach((f) => {
+				this.addHotkey(f)
+			})
+
+            this.writeToFile()
+		}
+
+		try {
+			fs.readFileSync(this.options.savefile)
+		} catch {
+			fs.createWriteStream(this.options.savefile)
+		}
+	}
+
+	addHotkey(hotkey: Hotkey) {
+		const hotkeysFile = this.readHotkeysFile()
+        console.log("Add: " + hotkeysFile.toString());
+
+		hotkeysFile.forEach((f) => {
+			if (f.label === hotkey.label) {
+				hotkey.shortcut = f.shortcut
+			}
+		})
+
+		this.hotkeys.push(hotkey)
+	}
+
+	reloadHotkeysFromFile() {
+		const hotkeysFile: Hotkey[] = this.readHotkeysFile()
+		this.hotkeys.forEach((currentHotkey) => {
+			hotkeysFile.forEach((newHotkey) => {
+				if (currentHotkey.label === newHotkey.label) {
+					currentHotkey.shortcut = newHotkey.shortcut
+				}
+			})
+		})
+	}
+
+	private readHotkeysFile(): Hotkey[] {
+		let hotkeysFile: Hotkey[] = []
+		try {
+            const hotkeyFileData = fs.readFileSync(this.options.savefile)
+            console.log(hotkeyFileData.toString())
+			hotkeysFile = JSON.parse(fs.readFileSync(this.options.savefile).toString())
+		} catch (e) {
+			hotkeysFile = JSON.parse("[]")
+		}
+
+		return hotkeysFile
+	}
+
+	registerHotkeysGlobal() {
+		this.hotkeys.forEach((key) => {
+			if (key.shortcut) {
+				key.registerHotkeyGlobal()
+			}
+		})
+	}
+
+	unregisterHotkeysGlobal() {
+		this.hotkeys.forEach((key) => {
+			key.unregisterHotkeyGlobal()
+		})
+	}
+
+	displayMenu() {
+		const win = new remote.BrowserWindow({
+			width: this.options.width,
+			height: this.options.width,
+			autoHideMenuBar: true,
+			webPreferences: {
+				enableRemoteModule: true,
+				nodeIntegration: true,
+			},
+		})
+
+
+		this.loadMenuLogic(win)
+	}
+
+	private loadMenuLogic(win: BrowserWindow) {
+		win.setMenu(null)
+		win.loadFile(
+			path.join(
+				pkgDir.sync(),
+				"node_modules/electron-hotkey-menu/resources/menu.html"
+			)
+		)
+
+		win.on("close", (e: Event) => {
+			this.unregisterHotkeysGlobal()
+
+			this.reloadHotkeysFromFile()
+
+			this.registerHotkeysGlobal()
+		})
+
+        this.sendHotkeyFileToMenu()
+		this.sendCSSToMenu()
+	}
+
+    private sendHotkeyFileToMenu() {
+        remote.ipcMain.on("hotkey-file-request", (event, args) => {
+            event.sender.send("hotkey-file-reply", this.options.savefile)
+        })
     }
 
-    this.options = options;
+	private sendCSSToMenu() {
+		remote.ipcMain.on("css-request", (event, arg) => {
+			event.sender.send("css-reply", this.options.css)
+		})
+	}
 
-    try {
-      fs.readFileSync(this.options.savefile);
-    } catch {
-      fs.createWriteStream(this.options.savefile);
-    }
-  }
-
-  addHotkey(hotkey: Hotkey) {
-    const hotkeysFile = this.readHotkeysFile();
-
-    hotkeysFile.forEach((f) => {
-      if (f.label === hotkey.label) {
-        hotkey.shortcut = f.shortcut;
-      }
-    });
-
-    this.hotkeys.push(hotkey);
-
-    this.writeToFile();
-  }
-
-  reloadHotkeysFromFile() {
-    const hotkeysFile: Hotkey[] = this.readHotkeysFile();
-    this.hotkeys.forEach((currentHotkey) => {
-      hotkeysFile.forEach((newHotkey) => {
-        if (currentHotkey.label === newHotkey.label) {
-          currentHotkey.shortcut = newHotkey.shortcut;
-        }
-      });
-    });
-  }
-
-  private readHotkeysFile(): Hotkey[] {
-    let hotkeysFile: Hotkey[] = [];
-    try {
-      hotkeysFile = JSON.parse(fs.readFileSync(this.options.savefile).toString());
-    } catch (e) {
-      hotkeysFile = JSON.parse('[]');
-    }
-
-    return hotkeysFile;
-  }
-
-  registerHotkeysGlobal() {
-    this.hotkeys.forEach((key) => {
-      if (key.shortcut) {
-        key.registerHotkeyGlobal();
-      }
-    });
-  }
-
-  unregisterHotkeysGlobal() {
-    this.hotkeys.forEach((key) => {
-      key.unregisterHotkeyGlobal();
-    });
-  }
-
-  displayMenu() {
-    const win = new remote.BrowserWindow({
-      width: this.options.width,
-      height: this.options.width,
-      autoHideMenuBar: true,
-      webPreferences: {
-        enableRemoteModule: true,
-        nodeIntegration: true,
-      },
-    });
-
-    this.loadMenuLogic(win);
-  }
-
-  private loadMenuLogic(win: BrowserWindow) {
-    win.setMenu(null);
-    win.loadFile(path.join(pkgDir.sync(), 'node_modules/electron-hotkey-menu/resources/menu.html'));
-
-    win.on('close', (e: Event) => {
-      this.unregisterHotkeysGlobal();
-
-      this.reloadHotkeysFromFile();
-
-      this.registerHotkeysGlobal();
-    });
-
-    this.sendCSSToMenu();
-  }
-
-  private sendCSSToMenu() {
-    remote.ipcMain.on('css-request', (event, arg) => {
-      event.sender.send('css-reply', this.options.css);
-    });
-  }
-
-  private writeToFile() {
-    const data = JSON.stringify(this.hotkeys);
-    fs.writeFileSync(this.options.savefile, data);
-  }
+	private writeToFile() {
+		const data = JSON.stringify(this.hotkeys)
+        console.log("Data: " + data);
+        
+		fs.writeFileSync(this.options.savefile, data)
+	}
 }
 
 // Holds a single hotkey
 export class Hotkey {
-  label: string;
-  shortcut: Electron.Accelerator;
-  onPress: () => any;
+	label: string
+	shortcut: Electron.Accelerator
+	onPress: () => any
 
-  constructor(label: string, key: Electron.Accelerator = '', onPress: () => any) {
-    this.onPress = onPress;
-    this.label = label;
-    this.shortcut = key;
-  }
+	constructor(
+		label: string,
+		key: Electron.Accelerator = "",
+		onPress: () => any
+	) {
+		this.onPress = onPress
+		this.label = label
+		this.shortcut = key
+	}
 
-  registerHotkeyGlobal() {
-    if (this.shortcut) {
-      remote.globalShortcut.register(this.shortcut, this.onPress);
-    }
-  }
+	registerHotkeyGlobal() {
+		if (this.shortcut) {
+			remote.globalShortcut.register(this.shortcut, this.onPress)
+		}
+	}
 
-  unregisterHotkeyGlobal() {
-    if (this.shortcut) {
-      remote.globalShortcut.unregister(this.shortcut);
-    }
-  }
+	unregisterHotkeyGlobal() {
+		if (this.shortcut) {
+			remote.globalShortcut.unregister(this.shortcut)
+		}
+	}
 }
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,167 +1,194 @@
-import { ipcRenderer, ipcMain, remote } from 'electron';
-import * as fs from 'fs';
-import * as path from 'path';
-import { Hotkey } from './hotkey-menu-launcher';
-import * as pkgDir from 'pkg-dir';
+import { ipcRenderer, ipcMain, remote } from "electron"
+import * as fs from "fs"
+import * as path from "path"
+import { Hotkey } from "./hotkey-menu-launcher"
+import * as pkgDir from "pkg-dir"
 
-const modifiers = [16, 17, 18, 91, 225];
+const modifiers = [16, 17, 18, 91, 225]
 const specialKeys: Record<number, string> = {
-  32: 'Space',
-};
+	32: "Space",
+}
+
+let hotkeys : Hotkey[]
+let hotkeyFilePath : string
 
 function initMenu() {
-  // Get hotkeys
-  const hotkeys: Hotkey[] = JSON.parse(fs.readFileSync('hotkeys.json').toString());
+    gatherHotkeyPathFromMain().then((result : string) => {
+        hotkeyFilePath = result
+        hotkeys = JSON.parse(fs.readFileSync(hotkeyFilePath).toString())
 
-  // Create table
-  const body = document.getElementsByTagName('body')[0];
-  const table = document.createElement('table');
-  const tbody = document.createElement('tbody');
+        document.getElementsByTagName("body")[0].prepend(buildHotkeyTable())
 
-  // Build hotkey field
-  hotkeys.forEach((f) => {
-    const tr = document.createElement('tr');
-
-    // Build label
-    const label = document.createElement('td');
-    label.innerHTML = f.label;
-    tr.appendChild(label);
-
-    // Build clear button
-
-    // Build input
-    const inputTd = document.createElement('td');
-    const input = document.createElement('input');
-
-    const buttonTd = document.createElement('td');
-    const button = document.createElement('button');
-
-    inputTd.width = '40%';
-    button.innerHTML = 'Clear';
-    inputTd.appendChild(input);
-    buttonTd.appendChild(button);
-    tr.appendChild(buttonTd);
-    tr.appendChild(inputTd);
-
-    // Setup inputfield logic
-    initInputField(f, input, button);
-
-    tbody.appendChild(tr);
-  });
-
-  // Build table
-  table.appendChild(tbody);
-  body.prepend(table);
-  initButtons(hotkeys);
-
-  loadCustomCSSIfExists();
+        initButtons()
+        loadCustomCSSIfExists()
+    })
 }
 
-function initInputField(hotkey: Hotkey, input: HTMLInputElement, clear: HTMLButtonElement) {
-  input.readOnly = true;
-  input.value = hotkey.shortcut.toString();
+function gatherHotkeyPathFromMain() {
+	return new Promise((resolve) => {
+		ipcRenderer.send("hotkey-file-request", ["hotkeys"])
 
-  const onPressFunc = (event: KeyboardEvent) => {
-    onKeyPress(hotkey, input, event);
-  };
-
-  input.onfocus = () => {
-    document.addEventListener('keydown', onPressFunc);
-  };
-
-  input.onblur = () => {
-    document.removeEventListener('keydown', onPressFunc);
-  };
-
-  clear.onclick = () => {
-    hotkey.shortcut = '';
-    input.value = '';
-  };
+		ipcRenderer.on("hotkey-file-reply", (event, args) => {
+			resolve(args)
+		})
+	})
 }
 
-function onKeyPress(hotkey: Hotkey, input: HTMLInputElement, event: KeyboardEvent) {
-  // Ignore only modifier keypresses
-  if (modifiers.includes(event.keyCode)) {
-    return;
-  }
+function buildHotkeyTable() : HTMLTableElement {
+	// Create table
+	const table : HTMLTableElement = document.createElement("table")
+	const tbody = document.createElement("tbody")
 
-  hotkey.shortcut = '';
+	// Build table row for each hotkey
+	hotkeys.forEach((hotkey) => {
+        const tr = buildTableRow(hotkey)
+		tbody.appendChild(tr)
+        initInputField(hotkey, tr)
+	})
 
-  if (event.metaKey) {
-    hotkey.shortcut += 'Super+';
-  }
+    table.appendChild(tbody)
 
-  if (event.ctrlKey) {
-    hotkey.shortcut += 'CmdOrCtrl+';
-  }
-
-  if (event.altKey) {
-    hotkey.shortcut += 'Alt+';
-  }
-
-  if (event.shiftKey) {
-    hotkey.shortcut += 'Shift+';
-  }
-
-  if (event.keyCode in specialKeys) {
-    hotkey.shortcut += specialKeys[event.keyCode];
-  } else {
-    hotkey.shortcut += event.key;
-  }
-
-  input.value = hotkey.shortcut.toString();
+    return table
 }
 
-function initButtons(hotkeys: Hotkey[]) {
-  const cancel = document.getElementById('cancel');
-  const confirm = document.getElementById('confirm');
+function buildTableRow(hotkey : Hotkey) : HTMLTableRowElement {
+		const tr = document.createElement("tr")
 
-  cancel.onclick = () => {
-    remote.getCurrentWindow().close();
-  };
+		// Build label
+		const label = document.createElement("td")
+		label.innerHTML = hotkey.label
+		tr.appendChild(label)
 
-  confirm.onclick = () => {
-    fs.writeFileSync('hotkeys.json', JSON.stringify(hotkeys));
-    remote.getCurrentWindow().close();
-  };
+
+		// Build input
+		const inputTd = document.createElement("td")
+		const input = document.createElement("input")
+
+		const buttonTd = document.createElement("td")
+		const button = document.createElement("button")
+
+		inputTd.width = "40%"
+		button.innerHTML = "Clear"
+		inputTd.appendChild(input)
+		buttonTd.appendChild(button)
+		tr.appendChild(buttonTd)
+		tr.appendChild(inputTd)
+
+        return tr
+}
+
+function initInputField(hotkey: Hotkey, row : HTMLTableRowElement) {
+    const input = row.getElementsByTagName("input")[0]
+    const clear = row.getElementsByTagName("button")[0]
+
+	input.readOnly = true
+	input.value = hotkey.shortcut.toString()
+
+	const onPressFunc = (event: KeyboardEvent) => {
+		onKeyPress(hotkey, input, event)
+	}
+
+	input.onfocus = () => {
+		document.addEventListener("keydown", onPressFunc)
+	}
+
+	input.onblur = () => {
+		document.removeEventListener("keydown", onPressFunc)
+	}
+
+	clear.onclick = () => {
+		hotkey.shortcut = ""
+		input.value = ""
+	}
+}
+
+function onKeyPress(
+	hotkey: Hotkey,
+	input: HTMLInputElement,
+	event: KeyboardEvent
+) {
+	// Ignore only modifier keypresses
+	if (modifiers.includes(event.keyCode)) {
+		return
+	}
+
+	hotkey.shortcut = ""
+
+	if (event.metaKey) {
+		hotkey.shortcut += "Super+"
+	}
+
+	if (event.ctrlKey) {
+		hotkey.shortcut += "CmdOrCtrl+"
+	}
+
+	if (event.altKey) {
+		hotkey.shortcut += "Alt+"
+	}
+
+	if (event.shiftKey) {
+		hotkey.shortcut += "Shift+"
+	}
+
+	if (event.keyCode in specialKeys) {
+		hotkey.shortcut += specialKeys[event.keyCode]
+	} else {
+		hotkey.shortcut += event.key
+	}
+
+	input.value = hotkey.shortcut.toString()
+}
+
+function initButtons() {
+	const cancel = document.getElementById("cancel")
+	const confirm = document.getElementById("confirm")
+
+	cancel.onclick = () => {
+		remote.getCurrentWindow().close()
+	}
+
+	confirm.onclick = () => {
+		fs.writeFileSync(hotkeyFilePath, JSON.stringify(hotkeys))
+		remote.getCurrentWindow().close()
+	}
 }
 
 function loadCustomCSSIfExists() {
-  const cssFilename = gatherCSSPathFromMain();
+	const cssFilename = gatherCSSPathFromMain()
 
-  cssFilename.then((result) => {
-    // If css path is not undefined, load it
-    if (result) {
-      loadCSSIntoWindow(result);
-    }
-  });
+	cssFilename.then((result) => {
+		// If css path is not undefined, load it
+		if (result) {
+			loadCSSIntoWindow(result)
+		}
+	})
 }
 
 // Get CSS from Main via ipc
 function gatherCSSPathFromMain(): Promise<string> {
-  return new Promise((resolve) => {
-    ipcRenderer.send('css-request', ['hotkeys']);
+	return new Promise((resolve) => {
+		ipcRenderer.send("css-request", ["hotkeys"])
 
-    ipcRenderer.on('css-reply', (event, args) => {
-      resolve(args);
-    });
-  });
+		ipcRenderer.on("css-reply", (event, args) => {
+			resolve(args)
+		})
+	})
 }
 
 // Load CSS into the menu. Filepath is relative to node project.
 function loadCSSIntoWindow(relativePath: string) {
-  const absolutePath = path.join(pkgDir.sync(), relativePath);
+	const absolutePath = path.join(pkgDir.sync(), relativePath)
 
-  const head = document.getElementsByTagName('HEAD')[0];
-  // Create new link Element
-  const link = document.createElement('link');
+	const head = document.getElementsByTagName("HEAD")[0]
+	// Create new link Element
+	const link = document.createElement("link")
 
-  // set the attributes for link element
-  link.rel = 'stylesheet';
-  link.type = 'text/css';
-  link.href = absolutePath;
-  // Append link element to HTML head
-  head.appendChild(link);
+	// set the attributes for link element
+	link.rel = "stylesheet"
+	link.type = "text/css"
+	link.href = absolutePath
+	// Append link element to HTML head
+	head.appendChild(link)
 }
 
-initMenu();
+initMenu()
